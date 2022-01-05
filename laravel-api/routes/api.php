492c629e0255b4d7a22c6808eb3d2ad6171833f5
InJoon-L <dlswns619@naver.com>
2022-01-05 14:47:29 +0900

    cors 적용

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -14,46 +14,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// 이런식으로 미들웨어 적용해야 에러 안남
-Route::prefix("users")->group(function () {
-    // token이 필요없는 route
-    Route::post("/register", [JWTAuthController::class, 'register']);
-    Route::post("/login", [JWTAuthController::class, 'login']);
-
-    // token이 필요한 route
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::get("/user", [JWTAuthController::class, 'user']);
-        Route::get("/logout", [JWTAuthController::class, 'logout']);
+Route::middleware(['cors'])->group(function() {
+    Route::get('/csrf_token', function(){
+        return csrf_token();
     });
+
+    // 이런식으로 미들웨어 적용해야 에러 안남
+    Route::prefix("users")->group(function () {
+        // token이 필요없는 route
+        Route::post("/register", [JWTAuthController::class, 'register']);
+        Route::post("/login", [JWTAuthController::class, 'login']);
+
+        // token이 필요한 route
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::get("/user", [JWTAuthController::class, 'user']);
+            Route::get("/logout", [JWTAuthController::class, 'logout']);
+        });
+    });
+
+    // 이런식으로 미들웨어 적용시 에러
+    // Route::prefix("users")->group(['middleware' => 'auth:api'], function () {
+    //     Route::get("/user", [JWTAuthController::class, 'user']);
+    //     Route::get("/logout", [JWTAuthController::class, 'logout']);
+    // });
+
+    Route::prefix("rooms")->group(function () {
+        // token이 필요없는 route
+
+        // token이 필요한 route
+        Route::group(['middleware' => 'auth:api'], function () {
+
+        });
+    });
+
+    Route::prefix("posts")->group(function () {
+        // token이 필요없는 route
+
+        // token이 필요한 route
+        Route::group(['middleware' => 'auth:api'], function () {
+
+        });
+    });
+
+    Route::get('unauthorized', function() {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Unauthorized'
+        ], 401);
+    })->name('api.jwt.unauthorized');
 });
 
-// 이런식으로 미들웨어 적용시 에러
-// Route::prefix("users")->group(['middleware' => 'auth:api'], function () {
-//     Route::get("/user", [JWTAuthController::class, 'user']);
-//     Route::get("/logout", [JWTAuthController::class, 'logout']);
-// });
 
-Route::prefix("rooms")->group(function () {
-    // token이 필요없는 route
-
-    // token이 필요한 route
-    Route::group(['middleware' => 'auth:api'], function () {
-
-    });
-});
-
-Route::prefix("posts")->group(function () {
-    // token이 필요없는 route
-
-    // token이 필요한 route
-    Route::group(['middleware' => 'auth:api'], function () {
-
-    });
-});
-
-Route::get('unauthorized', function() {
-    return response()->json([
-        'status' => 'error',
-        'message' => 'Unauthorized'
-    ], 401);
-})->name('api.jwt.unauthorized');
