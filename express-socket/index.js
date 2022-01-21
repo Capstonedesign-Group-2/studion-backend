c87e8fc4c75054a035684386b364858437c37353
InJoon-L <dlswns619@naver.com>
2022-01-22 00:30:11 +0900

    EDIT: cors 문제 해결 및 promise값 반환처리

--- express-socket/index.js
+++ express-socket/index.js
@@ -3,34 +3,58 @@ const http = require('http');
 const app = express();
 const cors = require('cors');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
 
-// import { createRoom } from './api/Room';
-const { createRoom, exitRoom } = require('./api/Room.js');
-
-const PORT = process.env.PORT || 3000;
+const obj = require('./api/Room.js');
 
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
+
 let users = {};
 let socketToRoom = {};
-let res;
 let roomID;
+let res;
+let func;
 
 const maximum = process.env.MAXIMUM || 4;
+
+app.set('io', io);
 const room = io.of('/room');
 const chat = io.of('/chat');
 
-// socket 연결
-room.on('connection', socket => {
+// room socket 연결
+io.on('connection', socket => {
+
+    socket.on('show', () => {
+        // promise가 반환되기때문에
+        // 동기처리를 해줘야 데이터가
+        // 제대로 넘어온다.
+        // 하루걸림
+        func = async() => {
+            res = await obj.roomList();
+
+            io.emit('show_on', res);
+        }
+
+        func();
+    });
+
+    console.log('room connection');
     // 방 입장
     socket.on('join_room', data => {
         // data set 
         // token: 토큰, data: room만들때 데이터, name: user_name,
         // room: room_id -> 방이 있을 시에만,
         // flag: 1 or 0 -> 1이면 방 입장, 0이면 방 만들고 입장
-        
         // 만들어진 방 입장 시
+        let res;
         if (data.flag) {
             roomID = data.room;
             const length = users[roomID].length;
@@ -43,7 +67,8 @@ room.on('connection', socket => {
             users[roomID].push({id: socket.id, name: data.name});
         } else {
             // 방 만들고 입장 시
-            res = createRoom(data.token, data.data);
+            res = obj.createRoom(data.token, data.data);
+            console.log(res.data);
 
             if (res.data.status === 'success') {
                 roomID = res.data.room.id;  
@@ -53,7 +78,7 @@ room.on('connection', socket => {
                 return;
             }
         }
-        
+        console.log('join_room 3')
         socketToRoom[socket.id] = roomID;
         
         socket.join(roomID);
@@ -61,7 +86,7 @@ room.on('connection', socket => {
         const usersInThisRoom = users[roomID].filter(user => user.id !== socket.id);
 
         console.log(usersInThisRoom);
-
+    
         io.sockets.to(socket.id).emit('all_users', res.data);
     });
 
@@ -89,11 +114,11 @@ room.on('connection', socket => {
     });
 
     // 방 퇴장
-    socket.on('disconnect', () => {
+    socket.on('exit_room', () => {
         const roomID = socketToRoom[socket.id];
         let room = users[roomID];
 
-        res = exitRoom(data.token, data.roomId);
+        res = obj.exitRoom(data.token, data.roomId);
 
         if (res.data.status !== 'success') {
             return;
@@ -110,6 +135,10 @@ room.on('connection', socket => {
         }
 
         socket.to(roomID).emit('user_exit', {id: socket.id});
+    });
+
+    socket.on('disconnect', () => {
+        console.log('연결해제')
     });
 });
 
