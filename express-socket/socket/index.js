330d13cbc4a63057e3baeec13d2beb46939d6e1a
InJoon-L <dlswns619@naver.com>
2022-03-11 13:08:42 +0900

    [EDIT] change redis -> local

--- express-socket/socket/index.js
+++ express-socket/socket/index.js
@@ -1,16 +1,46 @@
-const redisApi = require('../api/redisRoom.js');
+// const redisApi = require('../api/redisRoom.js');
 const chat = require('./chat.js');
 
 module.exports = {
-    // join, create event 나눔
-    // 룸 정보는 socket이랑 redis 삭제안함 리스트보여줄때 length 0 빼기
     start: async (io) => {
         let users = {};
         let socketToRoom = {};
         
-        // let roomID;
-        // let res;
-        // let func;
+        let roomInfo = {
+            rooms: []
+        };
+        let userToRoom = {};
+        let roomCount = 1;
+        
+        let getList = () => {
+            if (roomInfo.rooms.length === 0) return roomInfo;
+            let res = roomInfo;
+
+            for (let i = 0; i < res.rooms.length; i++) {
+                if (userToRoom[res.rooms[i].id]) {
+                    res.rooms[i].users = userToRoom[res.rooms[i].id];
+                } else {
+                    res.rooms.splice(i, 1);
+                    i--;
+                }
+            }
+
+            return res;
+        }
+        let getInfo = (data) => {
+            let res = null;
+
+            for (let i = 0; i < roomInfo.rooms.length; i++) {
+                if (roomInfo.rooms[i].id === data.id) {
+                    roomInfo.rooms[i] = data;
+                    res = roomInfo.rooms[i];
+                    res.users = userToRoom[res.id];
+                    break;
+                }
+            }
+
+            return res;
+        }
 
         const maximum = process.env.MAXIMUM || 4;
 
@@ -19,22 +49,26 @@ module.exports = {
         // room socket 연결
         io.on('connection', async (socket) => {
             // room list 가져올 시 room 내부 사람들 정보도 가져옴
+            // clear
             socket.on('get_room_list', async () => {
-                let res = await redisApi.getRoomList();
+                let res = getList();
                 console.log(res);
                 io.to(socket.id).emit('get_room_list_on', res);
             });
 
             // 방 내부 사람들 
+            // clear
             socket.on('get_room_user', async (data) => {
                 // data = {
-                //    room_id: room1
+                //    room_id: 1
                 // }
-                let res = await redisApi.getRoomUser(data.room_id);
+                // let res = await redisApi.getRoomUser(data.room_id);
+                let res = userToRoom[data.room_id];
                 console.log(res);
                 io.to(socket.id).emit('get_room_user_on', res);
             });
 
+            // clear
             socket.on('create_room', async (data) => {
                 // hash: studion, key: data.room.id, value: data 
                     // data = {
@@ -45,14 +79,21 @@ module.exports = {
                     //     locked: 0
                     //     // locked: 1이면 password 까지
                     // }
-                let roomCount = await redisApi.getRoomCount();
+                // let roomCount = await redisApi.getRoomCount();
                 data['id'] = roomCount;
-                    
-                await redisApi.createRoom(data.id, data);
+                roomCount++;
+                // await redisApi.createRoom(data.id, data);
+                if (roomCount['rooms']) {
+                    roomInfo['rooms'].push(data);
+                } else {
+                    roomInfo['rooms'] = [data];
+                }
                 
+                console.log(roomInfo);
                 io.to(socket.id).emit('create_room_on', data);
             });
 
+            // clear
             socket.on('update_room_info', async (data) => {
                 // hash: studion, key: data.room.id, value: data 
                     // data = {
@@ -64,14 +105,17 @@ module.exports = {
                     //     locked: 0
                     //     // locked: 1이면 password 까지
                     // }
-                
-                await redisApi.createRoom(data.id, data);
-                let res = await redisApi.getRoomInfo(data.id);
+                // await redisApi.createRoom(data.id, data);
+                // let res = await redisApi.getRoomInfo(data.id);
+                let res = getInfo(data);
+
                 io.to(data.id).emit('update_room_info_on', res);
-                res = await redisApi.getRoomList();
+                // res = await redisApi.getRoomList();
+                res = getList();
                 socket.broadcast.emit('update_room_list_on', res);
             });
 
+            // clear
             socket.on('join_room', async (data) => {
                 // data = {
                 //     user: {
@@ -82,6 +126,7 @@ module.exports = {
                 //     },
                 //     room_id: 'room1'
                 // };
+
                 if (users[data.room_id]) {
                     const length = users[data.room_id].length;
                     if (length === maximum) {
@@ -106,52 +151,68 @@ module.exports = {
                 }
                 socketToRoom[socket.id] = data.room_id;
                 data.user['socket_id'] = socket.id;
-                // hash: room_id, key: socket_id, value: user_data 
-                await redisApi.joinRoom(data.room_id, socket.id, data.user);
+                // await redisApi.joinRoom(data.room_id, socket.id, data.user);
+                if (userToRoom[data.room_id]) {
+                    let roomUser = userToRoom[data.room_id];
+                    roomUser = roomUser.filter(user => user.id !== data.user.id);
+                    userToRoom[data.room_id] = roomUser;
+                    userToRoom[data.room_id].push(data.user);
+                } else {
+                    userToRoom[data.room_id] = [data.user];
+                }
                 socket.join(data.room_id);
                 console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
-
+                console.log(userToRoom);
                 const usersInThisRoom = users[data.room_id].filter(user => user.id !== socket.id);
-
+                
                 console.log('usersInThisRoom', usersInThisRoom);
-
+                
                 io.sockets.to(socket.id).emit('all_users', usersInThisRoom);
-
-                let res = await redisApi.getRoomList();
+                
+                // let res = await redisApi.getRoomList();
+                let res = getList();
+                console.log(res);
                 io.emit('update_room_list_on', res);
             });
 
             // 합주실 리스트 업데이트 알림
+            // clear
             socket.on('update_room_list', async () => {
                 console.log('[ON] update_room_list');
-                let res = await redisApi.getRoomList();
+                let res = getList();
                 io.emit('update_room_list_on', res);
             });
 
             // 합주실 내부에 있는 유저들에게 정보 업데이트 알림
+            // clear
             socket.on('get_room_info', async (data) => {
-                let res = await redisApi.getRoomInfo(data.id);
+                // let res = await redisApi.getRoomInfo(data.id);
+                let res = getInfo(data);
                 io.to(data.id).emit('update_room_info_on', res);
             });
 
             // 시작 시 합주실 리스트 가져오기
-            socket.on('get_rooms', async (data) => {
-                let res = await redisApi.getRooms();
+            // clear
+            socket.on('get_rooms', async () => {
+                let res = getList();
                 io.to(socket.id).emit('get_rooms_on', res);
             });
 
             // 유저가 합주실을 나갔을 때
+            // clear
             socket.on('exit_room', async () => {
                 console.log('[ON] exit room', socket.id);
                 const roomID = socketToRoom[socket.id];
                 let room = users[roomID];
                 if (room) {
-                    let res = await redisApi.exitRoom(roomID, socket.id);
-                    if (res) {
-                        room = room.filter(user => user.id !== socket.id);
-                        users[roomID] = room;
-                        socket.leave(roomID);
-                    }
+                    // let res = await redisApi.exitRoom(roomID, socket.id);
+                    let roomUser = userToRoom[roomID]
+                    roomUser = roomUser.filter(user => user.socket_id !== socket.id);
+                    userToRoom[roomID] = roomUser;
+
+                    room = room.filter(user => user.id !== socket.id);
+                    users[roomID] = room;
+                    socket.leave(roomID);
                     // if (room.length === 0) {
                     //     let res = await redisApi.destoryRoom(roomID);
                     //     if (res) {
@@ -162,7 +223,7 @@ module.exports = {
                     // }
                 }
                 socket.to(roomID).emit('user_exit', { id: socket.id });
-                let res = await redisApi.getRoomList();
+                let res = getList();
                 socket.broadcast.emit('update_room_list_on', res);
             })
 
@@ -195,17 +256,20 @@ module.exports = {
                 socket.to(data.candidateReceiveID).emit('getCandidate', { candidate: data.candidate, candidateSendID: data.candidateSendID });
             });
 
+            // clear
             socket.on('disconnect', async () => {
                 console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
                 const roomID = socketToRoom[socket.id];
                 let room = users[roomID];
                 if (room) {
-                    let res = await redisApi.exitRoom(roomID, socket.id);
-                    if (res) {
-                        room = room.filter(user => user.id !== socket.id);
-                        users[roomID] = room;
-                        socket.leave(roomID);
-                    }
+                    // let res = await redisApi.exitRoom(roomID, socket.id);
+                    let roomUser = userToRoom[roomID]
+                    roomUser = roomUser.filter(user => user.socket_id !== socket.id);
+                    userToRoom[roomID] = roomUser;
+
+                    room = room.filter(user => user.id !== socket.id);
+                    users[roomID] = room;
+                    socket.leave(roomID);
                     // if (room.length === 0) {
                     //     let res = await redisApi.destoryRoom(roomID);
                     //     if (res) {
@@ -215,7 +279,7 @@ module.exports = {
                     //     }
                     // }
                     socket.to(roomID).emit('user_exit', { id: socket.id });
-                    let rooms = await redisApi.getRoomList();
+                    let rooms = getList();
                     socket.broadcast.emit('update_room_list_on', rooms);
                 }
                 console.log('[disconnect]', users);
